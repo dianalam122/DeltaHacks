e3a06ad88f3ff9384d8b9bbf76e3470fedab4219
petersonguo <petersonguo@gmail.com>
2024-01-14 02:32:06 -0500

    rerender on change

--- Dashboard.js
+++ Dashboard.js
@@ -1,36 +1,44 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Alert, Pressable, StyleSheet, Text, View} from 'react-native';
 import {onAuthStateChanged, signOut} from "firebase/auth";
 import {doc, getDoc} from "firebase/firestore";
 import {auth, db} from "./App";
+import {useIsFocused} from "@react-navigation/native";
 
-export const Dashboard = ({navigation, route}) => {
+export const Dashboard = ({navigation}) => {
     const [uid, setUid] = React.useState("");
     const [email, setEmail] = React.useState("");
     const [name, setName] = React.useState("");
     const [hasWallet, setHasWallet] = React.useState(false);
     const [walletId, setWalletId] = React.useState("");
 
-    onAuthStateChanged(auth, async (user) => {
-        if (user) {
-            // User is signed in, see docs for a list of available properties
-            // https://firebase.google.com/docs/reference/js/auth.user
-            const uid = user.uid;
-            const docRef = doc(db, "User", uid);
-            const docSnap = await getDoc(docRef);
+    const isFocused = useIsFocused()
 
-            if (docSnap.exists()) {
-                Alert.alert("Document data:", docSnap.data());
-                setHasWallet(docSnap.data().walletId !== "");
-                setWalletId(docSnap.data().walletId);
-                setEmail(docSnap.data().email);
-                setUid(uid);
-                setName(docSnap.data().name);
-            }
-        } else {
-            navigation.navigate("Home");
+    useEffect(() => {
+        if (isFocused) {
+            onAuthStateChanged(auth, async (user) => {
+                if (user) {
+                    // User is signed in, see docs for a list of available properties
+                    // https://firebase.google.com/docs/reference/js/auth.user
+                    const uid = user.uid;
+                    const docRef = doc(db, "User", uid);
+                    const docSnap = await getDoc(docRef);
+
+                    if (docSnap.exists()) {
+                        Alert.alert("Document data:", docSnap.data());
+                        setHasWallet(docSnap.data().walletId !== "");
+                        setWalletId(docSnap.data().walletId);
+                        setEmail(docSnap.data().email);
+                        setUid(uid);
+                        setName(docSnap.data().name);
+                    }
+                } else {
+                    navigation.navigate("Home");
+                }
+            });
         }
-    });
+    }, [isFocused])
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Dashboard</Text>
